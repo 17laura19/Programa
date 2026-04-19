a3b5e7aac5e25c631e4f51d5a117c4e10ee7f751
17laura19 <17laura19@gmail.com>
2026-04-19 20:31:31 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'programa-v4';
+var CACHE_NAME = 'programa-v5';
 var URLS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
